e3c4abf63934ab4c1e0c7581c3d287d32183c672
Sadima Maharjan <sadimamaharjan@192-168-1-108.tpgi.com.au>
2023-01-12 19:56:10 +1100

    added functions to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 let topic = ["HTML", "CSS", "Git", "JavaScript"];
-/*
-if (topic === "HTML") {
-  console.log("Let's study HTML!");
-} else if (topic === "CSS") {
-  console.log("Let's study CSS!");
-} else if (topic === "Git") {
-  console.log("Let's study Git!");
-} else if (topic === "JavaScript") {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log("Please try again!");
-}
-*/
+let randomTopic = topic[Math.floor(Math.random() * topic.length)];
 
-for (let i = 0; i < topic.length; i++) {
-  console.log(`Let's study ${topic[i]}`);
+function listTopics() {
+  for (let i = 0; i < topic.length; i++) {
+    console.log(topic[i]);
+  }
 }
+
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === "Git") {
+    console.log("Let's study Git!");
+  } else if (randomTopic === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
